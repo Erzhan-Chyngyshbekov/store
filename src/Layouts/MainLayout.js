d557ea2c512af5c5e1281b815cf4754173e95445
erzhan-chyngyshbekov <e4yngywbekov@mail.ru>
2021-06-16 21:34:16 +0600

    first commit

---
+++ src/Layouts/MainLayout.js
@@ -0,0 +1,100 @@
+import React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import Box from "@material-ui/core/Box";
+import Container from "@material-ui/core/Container";
+import Slide from "@material-ui/core/Slide";
+import MenuBar from "../components/MenuBar/MenuBar";
+import SearchBar from "../components/SearchBar/SearchBar";
+import { makeStyles, useTheme } from "@material-ui/core";
+import { NavLink } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    minHeight: "100vh",
+    position: "relative",
+  },
+  contentShift: {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
+  navContent: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  logo: {
+    [theme.breakpoints.down("sm")]: {
+      display: "none",
+    },
+  },
+  toolbar: {
+    backgroundColor: "#2f363d",
+    borderBottom: "3px solid #e7315d",
+  },
+  main: {
+    backgroundColor: "#ecebeb",
+    marginTop: 90,
+    padding: 10,
+  },
+}));
+
+function HideOnScroll(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({ target: window ? window() : undefined });
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
+
+HideOnScroll.propTypes = {
+  children: PropTypes.element.isRequired,
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default function MainLayout(props) {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <HideOnScroll {...props}>
+        <AppBar>
+          <Toolbar className={classes.toolbar}>
+            <MenuBar />
+            <div className={classes.navContent}>
+              <Typography className={classes.logo} variant="h6">
+                TechnoStore
+              </Typography>
+              <SearchBar />
+            </div>
+          </Toolbar>
+        </AppBar>
+      </HideOnScroll>
+      <main className={classes.main}>{props.children}</main>
+    </React.Fragment>
+  );
+}
---
+++ src/components/HeroSlider/HeroSlider.js
@@ -0,0 +1,122 @@
+import React from "react";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import MobileStepper from "@material-ui/core/MobileStepper";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
+import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
+import SwipeableViews from "react-swipeable-views";
+import { autoPlay } from "react-swipeable-views-utils";
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+const tutorialSteps = [
+  {
+    imgPath: "https://www.myphone.kg/files/media/12/12155.jpg",
+  },
+  {
+    imgPath: "https://www.myphone.kg/files/media/12/12129.jpg",
+  },
+  {
+    imgPath: "https://www.myphone.kg/files/media/11/11993.jpg",
+  },
+  {
+    imgPath: "https://www.myphone.kg/files/media/11/11688.jpg",
+  },
+  {
+    imgPath: "https://www.myphone.kg/files/media/11/11991.jpg",
+  },
+];
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 1200,
+    flexGrow: 1,
+    margin: "30px auto",
+  },
+  header: {
+    display: "flex",
+    alignItems: "center",
+    height: 50,
+    paddingLeft: theme.spacing(4),
+    backgroundColor: theme.palette.background.default,
+  },
+  img: {
+    display: "block",
+    overflow: "hidden",
+    width: "100%",
+  },
+}));
+
+export default function HeroSlider() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const maxSteps = tutorialSteps.length;
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleStepChange = (step) => {
+    setActiveStep(step);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AutoPlaySwipeableViews
+        axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+        index={activeStep}
+        onChangeIndex={handleStepChange}
+        enableMouseEvents
+      >
+        {tutorialSteps.map((step, index) => (
+          <div key={step.label}>
+            {Math.abs(activeStep - index) <= 2 ? (
+              <img
+                className={classes.img}
+                src={step.imgPath}
+                alt={step.imgPath}
+              />
+            ) : null}
+          </div>
+        ))}
+      </AutoPlaySwipeableViews>
+      <MobileStepper
+        steps={maxSteps}
+        position="static"
+        variant="text"
+        activeStep={activeStep}
+        nextButton={
+          <Button
+            size="small"
+            onClick={handleNext}
+            disabled={activeStep === maxSteps - 1}
+          >
+            Next
+            {theme.direction === "rtl" ? (
+              <KeyboardArrowLeft />
+            ) : (
+              <KeyboardArrowRight />
+            )}
+          </Button>
+        }
+        backButton={
+          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+            {theme.direction === "rtl" ? (
+              <KeyboardArrowRight />
+            ) : (
+              <KeyboardArrowLeft />
+            )}
+            Back
+          </Button>
+        }
+      />
+    </div>
+  );
+}
